d285a728bc8c69f7a162b5ef5081a3f98241d073
Subhasis Nanda <your.email@example.com>
2026-01-05 14:59:56 +0530

    changes

--- dsa/arrays-strings/0013-single-number/Solution.cs
+++
@@ -1,9 +0,0 @@
-public class Solution
-{
-    public int SingleNumber(int[] nums)
-    {
-        int ans = 0;
-        foreach (var x in nums) ans ^= x;
-        return ans;
-    }
-}
--- dsa/linked-lists/0008-delete-node-in-a-linked-list/Solution.cs
+++
@@ -1,9 +0,0 @@
-public class Solution
-{
-    public void DeleteNode(ListNode node)
-    {
-        // node is guaranteed not to be the tail
-        node.val = node.next.val;
-        node.next = node.next.next;
-    }
-}
